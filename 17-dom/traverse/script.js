919ae11821e7a869aea997f856abd344787d2ed7
TechGlobalSchool <tgschool01@gmail.com>
2024-09-28 10:36:45 -0500

    dom traverse

---
+++ 17-dom/traverse/script.js
@@ -0,0 +1,48 @@
+// Recap
+// Ways of accessing elements in DOM
+// by id: getElementById()
+// by class: getElementsByClassName()
+// by tag name: getElementsByTagName()
+// universal: querySelector(), querySelectorAll()
+
+/**
+ * TRAVERSING THE DOM
+ */
+
+const parentEl = document.getElementById('parent');
+
+const child1El = document.getElementById('child1');
+const child2El = document.getElementById('child2');
+const child3El = document.getElementById('child3');
+
+/* --- 1. Node only */
+
+// parentNode
+console.log(child1El.parentNode); // parent div
+console.log(parentEl.parentNode); // body
+
+// childNodes
+const childNodes = parentEl.childNodes;
+console.log(childNodes); // NodeList(7) [text, p#child1, text, p#child2, text, p#child3, text]
+
+// firstChild, lastChild
+console.log('First Child', parentEl.firstChild) // text
+console.log('Last Child', parentEl.lastChild) // text
+
+// previousSibling, nextSibling
+console.log('Previous sibling to child1', child1El.previousSibling); // text
+console.log('Next sibling to child 1', child1El.nextSibling); // text
+
+/* --- 2. Elements only */
+
+// parentElement
+console.log('Parent Element of child 1', child1El.parentElement); // parent div
+console.log('Parent Element of parent div', parentEl.parentElement); // body
+
+// children
+console.log('Children of parent div', parentEl.children); // HTMLCollection(3) [p#child1, p#child2, p#child3, child1: p#child1, child2: p#child2, child3: p#child3]
+
+// firstElementChild, lastElementChild
+console.log('First Element Child', parentEl.firstElementChild) // ?
+console.log('Last Element Child', parentEl.lastElementChild) // ?
+
